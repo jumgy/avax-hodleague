8138a70bb3313f17becebf7724f5641dc748e144
jumgy <kruglyakovanton@gmail.com>
2025-11-15 18:43:14 +0300

    add zk instead of dexe

--- data/game_tokens.py
+++ data/game_tokens.py
@@ -30,7 +30,7 @@ TOKEN_WEIGHTS = {
     'FLR': 2, 'WLFI': 2, 'SPX': 2,
     
     # Вес 1 (3 токена) - Максимальный риск/потенциал
-    'DEXE': 1, 'KAIA': 1, 'M': 1
+    'ZK': 1, 'KAIA': 1, 'M': 1
 }
 
 GAME_TOKENS = list(TOKEN_WEIGHTS.keys())
