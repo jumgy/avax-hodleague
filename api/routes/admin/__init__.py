9f4de91ece2207ff2bd3eafa8c09d0db2c54c115
jumg <kruglyakovanton@gmail.com>
2025-12-08 21:20:52 +0300

    add routes to init

--- api/routes/admin/__init__.py
+++ api/routes/admin/__init__.py
@@ -9,7 +9,10 @@ from .drop_rates import router as drop_rates_router
 from .tournaments import router as tournaments_router
 from .prizes import router as prizes_router
 from .users import router as users_router 
+from .user_cards import router as user_cards_router
+from .user_packs import router as user_packs_router
 from .tournament_details import router as tournament_details_router
+from .reward import router as reward_router
 
 
 # Create admin router
@@ -20,12 +23,17 @@ admin_router.include_router(auth_router, tags=["Admin - Auth"])
 admin_router.include_router(tokens_router, tags=["Admin - Tokens"])
 admin_router.include_router(cards_router, tags=["Admin - Cards"])
 admin_router.include_router(rarities_router, tags=["Admin - Rarities"])
+admin_router.include_router(reward_router, tags=["Admin - Rewards"])
 admin_router.include_router(pack_types_router, tags=["Admin - Pack types"])
 admin_router.include_router(drop_rates_router, tags=["Admin - Drop rates"])
 admin_router.include_router(tournaments_router, tags=["Admin - Tournaments"])
 admin_router.include_router(tournament_details_router, tags=["Admin - Tournament details"])
 admin_router.include_router(prizes_router, tags=["Admin - Prizes"])
+
 admin_router.include_router(users_router, tags=["Admin - Users"])
+admin_router.include_router(user_cards_router, tags=["Admin - User Cards"])
+admin_router.include_router(user_packs_router, tags=["Admin - User Packs"])
+
 
 # Export router
 __all__ = ["admin_router"]
